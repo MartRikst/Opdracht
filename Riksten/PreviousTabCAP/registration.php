09823655366770f1b3110f1ec57aaccfb99b76f3
MartRikst <36696173+MartRikst@users.noreply.github.com>
2018-06-05 20:12:35 +0200

    Add files via upload

--- Riksten/PreviousTabCAP/registration.php
+++ Riksten/PreviousTabCAP/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Riksten_PreviousCAP',
+    'Riksten_PreviousTabCAP',
     __DIR__
 );
